d0fdd30027d0f5544a70e8f4ca5fc6958bddd195
Abhinav <abhinav@Abhuntu.(none)>
2013-10-08 03:24:33 -0400

    added pop_gen script

---
+++ pop_data_simulator.m
@@ -0,0 +1,34 @@
+%%%%
+%%% Abhinav Bajaj	
+%%%%
+
+noOfSubPop = 5;
+noOfPop = 3;
+noOfMarkerLoci = 10;
+freqParam = 1e2; %% 1000, Wright Fisher model
+noOfIndiv = 10;
+
+%%
+drawFrom  = ones(noOfSubPop,1);
+% ratios = [0.3;0.2;0.1;0.1;0.3]; % Ratios of contribution of each sub pop
+
+subPops = zeros(noOfSubPop,noOfMarkerLoci);
+for i = 1:noOfSubPop
+	subPops(i,:) = subPopulation_simulator(noOfMarkerLoci,freqParam);
+end
+disp(subPops); 
+disp('+++++++++++++++++++++++++++++++++');
+pops = zeros(noOfPop,noOfMarkerLoci);
+ratios = zeros(noOfSubPop,noOfPop);
+for i = 1: noOfPop
+	[pops(i,:),ratios(:,i)] = population_simulator(subPops,drawFrom);
+end
+
+disp(pops);
+disp('+++++++++++++++++++++++++++++++++');
+
+indiv = zeros(noOfPop,noOfIndiv,noOfMarkerLoci);
+for i = 1:noOfPop
+	indiv(i,:,:) = cohort_simulator(noOfIndiv,pops(i,:)) ; 
+end
+disp(indiv);
