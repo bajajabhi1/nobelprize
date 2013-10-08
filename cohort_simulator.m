959596ae5ecb47603caaf9fa4ac9185b69798c9b
Nathan  A Keane <nak2126@vanilla.clic.cs.columbia.edu>
2013-10-08 01:24:31 -0400

    cohort change

--- cohort_simulator.m
+++ cohort_simulator.m
@@ -1,11 +1,15 @@
-%%%Arpit Gupta
+%%%Arpit Gupta & nathan
 %n is the number of individuals you want to draw
 %%%pop_density is a row vector of size say, 1X10^6
 %%%
 
 function [cohort]  = cohort_simulator(n,pop_density)
-cohort = rand(n,length(pop_density),2) ; 
+     cohort = zeros(n, length(pop_density)); 
+allele_sample = rand(n,length(pop_density),2); 
+
+
 for j = 1:length(pop_density)
-	cohort(:,j,:) = cohort(:,j,:) <= pop_density(j) ; 
+	       cohort(:,j) = uint32(allele_sample(:,j,1) <= pop_density(j)) + uint32(allele_sample(:,j,2) <= pop_density(j)); 
 end
+
 	
