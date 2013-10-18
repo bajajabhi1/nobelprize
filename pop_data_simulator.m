fa5b6b454e2018cd9e257135bfd078666f5ecbe7
Abhinav <abhinav@Abhuntu.(none)>
2013-10-17 20:24:14 -0400

    Added changes for data_simulation format

--- pop_data_simulator.m
+++ pop_data_simulator.m
@@ -1,10 +1,14 @@
 %%%%
 %%% Abhinav Bajaj	%%%
+%%% Usage pop_data_simulator(outPopformat)
+%%%
+%%% outPopFormat - Output file format for Population
 %%%%
+function pop_data_simulator(outPopFormat)
 
 noOfSubPop = 5;
 noOfPop = 3;
-noOfMarkerLoci = 10000;
+noOfMarkerLoci = 1000;
 freqParam = 1e3; %% 1000, Wright Fisher model
 noOfIndiv = 1000;
 
@@ -24,13 +28,14 @@ for i = 1: noOfPop
 	[pops(i,:),ratios(:,i)] = population_simulator(subPops,drawFrom);
 end
 
-
 disp('Population Gene Densities Generated');
 
 indiv = zeros(noOfIndiv,noOfMarkerLoci,noOfPop);
 for i = 1:noOfPop
 	indiv(:,:,i) = cohort_simulator(noOfIndiv,pops(i,:)) ; 
 end
+%% write the output population generated
+writeToFile(indiv,outPopFormat);
 disp('Cohorts drawn from population densities');
 save('Population_data.mat','indiv','pops','subPops','ratios');
 
