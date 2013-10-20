90d2c74ab2c327925a6c7591413d599e997af7df
root <root@arpitg1991-HP-ENVY-15-Notebook-PC.(none)>
2013-10-20 00:05:38 -0400

    changing pop_data_simulator.m

--- pop_data_simulator.m
+++ pop_data_simulator.m
@@ -4,38 +4,52 @@
 %%%
 %%% outPopFormat - Output file format for Population
 %%%%
-function pop_data_simulator(outPopFormat)
 
-noOfSubPop = 5;
+noOfSubPop = 3;
 noOfPop = 3;
-noOfMarkerLoci = 10;
-freqParam = 1e3; %% 1000, Wright Fisher model
-noOfIndiv = 10;
+noOfMarkerLoci = 1000000;
+freqParam = 500; %% 1000, Wright Fisher model
+noOfIndiv = 500;
 
 %%
 drawFrom  = ones(noOfSubPop,1);
-% ratios = [0.3;0.2;0.1;0.1;0.3]; % Ratios of contribution of each sub pop
-
+ratios1 = [0.7;0.2;0.1]; % Ratios of contribution of each sub pop
+ratios2 = [0.4;0;0.6];
+ratios3 = [0.2;0.3;0.5];
+ratios = [ratios1 ratios2 ratios3] ;
 subPops = zeros(noOfSubPop,noOfMarkerLoci);
 for i = 1:noOfSubPop
 	subPops(i,:) = subPopulation_simulator(noOfMarkerLoci,freqParam);
 end
 
 disp('SubPopulations generated');
-pops = zeros(noOfPop,noOfMarkerLoci);
-ratios = zeros(noOfSubPop,noOfPop);
-for i = 1: noOfPop
-	[pops(i,:),ratios(:,i)] = population_simulator(subPops,drawFrom);
-end
+%pops = zeros(noOfPop,noOfMarkerLoci);
+%ratios = zeros(noOfSubPop,noOfPop);
+%for i = 1: noOfPop
+%	[pops(i,:),ratios(:,i)] = population_simulator(subPops,drawFrom);
+%end
 
 disp('Population Gene Densities Generated');
-
-indiv = zeros(noOfIndiv,noOfMarkerLoci,noOfPop);
+%whos
+%indiv = zeros(noOfIndiv,noOfMarkerLoci,noOfPop);
+indiv = zeros(noOfMarkerLoci,1);
+formatSpec = 'pos%d*%d ' ; 
+index = 1:1000000;
+%whos
 for i = 1:noOfPop
-	indiv(:,:,i) = cohort_simulator(noOfIndiv,pops(i,:)) ; 
+	%whos
+	pops = population_simulator(subPops,drawFrom,ratios(:,i));
+	for j = 1:noOfIndiv
+		filename = sprintf('files/pop%dindiv%d',i,j);
+		indiv = cohort_simulator(1,pops) ; 
+		fid = fopen(filename,'w');
+		fprintf(fid,formatSpec,[index;indiv]);
+		fclose(fid);
+		disp(filename);
+	end
 end
 %% write the output population generated
-writeToFile(indiv,outPopFormat);
+%writeToFile(indiv,outPopFormat);
 disp('Cohorts drawn from population densities');
-save('Population_data.mat','indiv','pops','subPops','ratios');
+%save('Population_data.mat','indiv','pops','subPops','ratios');
 
