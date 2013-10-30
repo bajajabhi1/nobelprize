e6a0bc5d2cbebc403cd0ddf54281400679b1c20f
Nathan  A Keane <nak2126@vanilla.clic.cs.columbia.edu>
2013-10-30 13:40:27 -0400

    Changed input parameter to be min frequency

--- subPopulation_simulator.m
+++ subPopulation_simulator.m
@@ -1,12 +1,13 @@
 %%%%%
 %Nathan Keane
 % Simulator with gene frequency = 1 / n;
-%%%%%
+% k = minimum frequency you want to draw;
+%%%%%%
 
-function [sample] = subPopulation_simulator(n,w)
-
-%sample = exp(w*rand(1,n)- w);
-sample = 1 ./ (unidrnd(w-1,1,n) + rand(1,n)) ;
+function [sample] = subPopulation_simulator(n,k)
+w = -log(k);
+sample = exp(w*rand(1,n)- w);
+%sample = 1 ./ (unidrnd(w-1,1,n) + rand(1,n)) ;
 
 
 
